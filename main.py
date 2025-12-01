8f9b7698eae6378cfe7f3388f244ee170b39abad
bax1e <beckham777rus@gmail.com>
2025-12-01 14:42:07 +0300

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import telebot
+import os
 
-# Вставь сюда свой токен ОТ BotFather, в кавычках
-TOKEN = "8394431801:AAH0_ojaFGruQ9LJQMJIEkXa9v0Gnbhjgfk"
+# Берём токен из переменной среды Railway
+TOKEN = os.getenv("TOKEN")
+
+if TOKEN is None:
+    raise ValueError("ERROR: переменная среды TOKEN не найдена! Добавь её в Railway → Variables.")
 
 bot = telebot.TeleBot(TOKEN)
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, "Привет! Я рабочий бот компании it-task 🚀")
 
+
 @bot.message_handler(func=lambda m: True)
 def echo(message):
     bot.reply_to(message, "Привет!")
+
 
 bot.infinity_polling()
